a932db30cca762ae4f94f1b090685472b5b651b2
kuriyamaEngineer <kuriyama.engineer041@gmail.com>
2024-03-19 09:27:47 +0900

    2024/03/19 kuriyama 修正

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++){
+  smoothScrollTrigger[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = smoothScrollTrigger[i].getAttribute('href');
+     let targetElement = document.getElementById(href.replace('#', ''));
+    const rect = targetElement.getBoundingClientRect().top;
+    const offset = window.pageYOffset;
+    const gap = 60;
+    const target = rect + offset - gap;
+    window.scrollTo({
+      top: target,
+    });
+  });
+}
+
 $(function(){
     /*=================================================
     Inview（画面に表示されたタイミングで処理を実行）
